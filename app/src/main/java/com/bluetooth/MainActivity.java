b30840dd939ebb6aa8d7853800db04d1e9ec5a50
Mateusz Dziubek <mateusz.dziubek@schibsted.pl>
2017-01-06 16:26:50 +0100

    DZIALA

--- app/src/main/java/com/bluetooth/MainActivity.java
+++ app/src/main/java/com/bluetooth/MainActivity.java
@@ -26,8 +26,7 @@ import java.util.UUID;
 public class MainActivity extends AppCompatActivity {
 
     private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
-    private final int RECIEVE_MESSAGE = 1;
-    private StringBuilder sb = new StringBuilder();
+    private final int RECEIVE_MESSAGE = 1;
 
     private TextView temp1TextView;
     private TextView temp2TextView;
@@ -36,9 +35,12 @@ public class MainActivity extends AppCompatActivity {
     private TextView feedbackTextView;
     private BluetoothSocket btSocket;
 
+    private List<BluetoothDevice> bluetoothDeviceList = new ArrayList<>();
+
     Handler h;
     int i;
     float t1;
+    private StringBuilder sb = new StringBuilder();
 
 
     @Override
@@ -53,16 +55,15 @@ public class MainActivity extends AppCompatActivity {
         feedbackTextView = (TextView) findViewById(R.id.feedback_text_view);
 
         h = new Handler() {
-            public void handleMessage(Message msg) {//Retrieve the a Handler implementation that will receive this message. The object must implement Handler.handleMessage(). Each Handler has its own name-space for message codes, so you do not need to worry about yours conflicting with other handlers.
-                //Wyżej implementacja funkcji ktora jest zwracana jako
-                switch (msg.what) {//tutaj w parametze wywołujemy Definiowany przez użytkownika kod komunikatu dieki czemu mozna ustalic czymjest wiadmoas. Czyli zwraca wartsc dzieki ktorej wykona sie nasz kod kiedy jest jakas wiadomosc
-                    case RECIEVE_MESSAGE:                                                   // if receive massage
+            public void handleMessage(Message msg) {
+                switch (msg.what) {
+                    case RECEIVE_MESSAGE:
                         byte[] readBuf = (byte[]) msg.obj;
-                        String strIncom = new String(readBuf, 0, msg.arg1);                 // create string from bytes array
-                        sb.append(strIncom);                                                // append string
-                        int endOfLineIndex = sb.indexOf("\r\n");                            // determine the end-of-line
-                        if (endOfLineIndex > 0) {                                            // if end-of-line,
-                            String sbprint = sb.substring(0, endOfLineIndex);               // extract string
+                        String strIncom = new String(readBuf, 0, msg.arg1);
+                        sb.append(strIncom);
+                        int endOfLineIndex = sb.indexOf("\r\n");
+                        if (endOfLineIndex > 0) {
+                            String sbprint = sb.substring(0, endOfLineIndex);
                             if (sb.indexOf("#") != (-1)) {
                                 i = sb.indexOf("#");
                                 // and clear
@@ -70,18 +71,16 @@ public class MainActivity extends AppCompatActivity {
 
                                     String zmienna = new String();
                                     i++;
-                                    while (sb.charAt(i) != '~')//Utworzenie stringa z nazwą zmiennej do zapisu
-                                    {
+                                    while (sb.charAt(i) != '~') {
                                         zmienna += sb.charAt(i++);
                                     }
                                     i++;
-                                    String cyfra = new String();//spytac dziubka czy muszę usować
+                                    String cyfra = new String();
                                     while (sb.charAt(i) != '#') {
                                         cyfra += sb.charAt(i++);
                                     }
                                     float f = Float.parseFloat(cyfra);
-                                    if (zmienna.equals("t1") == true)//Porównananie w celu przypisania do odpowiedniego stringa
-                                    {
+                                    if (zmienna.equals("t1") == true) {
                                         t1 = f;
                                     }
 
@@ -91,12 +90,7 @@ public class MainActivity extends AppCompatActivity {
                             }
                             sb.delete(0, sb.length());
                             feedbackTextView.setText("Data from Arduino: " + sbprint);
-                            // btnOff.setEnabled(true);//Nieuzywany
-                            // btnOn.setEnabled(true);//daje brak reakcji na klniecia
                         }
-                        //Log.d(TAG, "...String:"+ sb.toString() +  "Byte:" + msg.arg1 + "...");
-
-
                         break;
 
                 }
@@ -127,31 +121,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 // startRepeatingTask();
                 {
-                    btAdapter.startDiscovery();
-
-                    List<BluetoothDevice> searchList = new ArrayList<>();
-                    BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-                    Iterable<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
-
-                    for (BluetoothDevice bluetoothDevice : pairedDevices) {
-                        searchList.add(bluetoothDevice);
-                    }
                     try {
-                        btSocket = createBluetoothSocket(searchList.get(bluetoothSpinner.getSelectedItemPosition()));
+                        btSocket = createBluetoothSocket(bluetoothDeviceList.get(bluetoothSpinner.getSelectedItemPosition()));
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
 
-                    btAdapter.cancelDiscovery();
-
                     try {
                         btSocket.connect();
                     } catch (IOException e) {
-                        try {
-                            btSocket.close();
-                        } catch (IOException e2) {
-                            e2.printStackTrace();
-                        }
+                        e.printStackTrace();
                     }
 
                     ConnectedThread mConnectedThread = new ConnectedThread(btSocket);
@@ -190,6 +169,7 @@ public class MainActivity extends AppCompatActivity {
         Iterable<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
 
         for (BluetoothDevice bluetoothDevice : pairedDevices) {
+            bluetoothDeviceList.add(bluetoothDevice);
             bluetoothList.add(bluetoothDevice.getName());
         }
 
@@ -218,18 +198,16 @@ public class MainActivity extends AppCompatActivity {
         return device.createRfcommSocketToServiceRecord(MY_UUID);
     }
 
-    class ConnectedThread extends Thread {//nasza kalsa z watkiem
-        private final InputStream mmInStream;//strumien który możemy tylko modyfikowac jesli przekazujemy referencje
+    class ConnectedThread extends Thread {
+        private final InputStream mmInStream;
         private final OutputStream mmOutStream;
 
         public ConnectedThread(BluetoothSocket socket) {
             InputStream tmpIn = null;
             OutputStream tmpOut = null;
 
-            // Get the input and output streams, using temp objects because
-            // member streams are final
             try {
-                tmpIn = socket.getInputStream();//Tak bo chcemy zeby orginalane zostały nie modyfikowalne bo one sa cały czas używane
+                tmpIn = socket.getInputStream();
                 tmpOut = socket.getOutputStream();
             } catch (IOException e) {
             }
@@ -239,17 +217,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         public void run() {
-            byte[] buffer = new byte[256];  // buffer store for the stream
-            int bytes; // bytes returned from read()
+            byte[] buffer = new byte[256];
+            int bytes;
 
-            // Keep listening to the InputStream until an exception occurs
             while (true) {
                 try {
-                    // Read from the InputStream
-                    bytes = mmInStream.read(buffer);        // Get number of bytes and message in "buffer"
-
-                    //Tutaj Wysłanie do UI (UserInterface) Wyciągnięcie z wątku i wysłanie za pomoca metody sendToTarget(); dzięki temu że wcześniej ustawiliśmy msg.what - zmienini sie na 1
-                    h.obtainMessage(RECIEVE_MESSAGE, bytes, -1, buffer).sendToTarget();     // Send to message queue Handler
+                    bytes = mmInStream.read(buffer);
+                    h.obtainMessage(RECEIVE_MESSAGE, bytes, -1, buffer).sendToTarget();
                 } catch (IOException e) {
                     break;
                 }
